7b231a3da82aee19cb003b6bb4c8d9e48366bd1d
krushna <krushnak819@gmail.com>
2026-08-01 10:17:58 +0530

    Initial React project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>CareerPilot AI</h1>
+      <p>Your Intelligent Career Mentor & Learning Companion</p>
+    </div>
+  );
+}
+
+export default Home;
